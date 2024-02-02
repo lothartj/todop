ec52b4a0190d9129be98e9a3ceca397ac34681d3
Lothar Tjipueja <lotharwebdev@gmail.com>
2024-02-02 10:19:03 +0200

    New commit

--- todop/settings.py
+++ todop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vutp+^4z0*a@!%n%)oz*jg#^9onzi^k=*#3(bdja%n8vpp)*2%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lothart.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = '/home/yourusername/yourproject/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- todop/wsgi.py
+++ todop/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for todop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
+path = '/home/lothart/yourproject'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'todop.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todop.settings")
-
 application = get_wsgi_application()
